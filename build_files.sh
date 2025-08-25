747fea630da70982d9ddf9318ec98b605a85968a
Sanketmajithiya <Sanketmajithiya@gmail.com>
2025-08-25 12:49:36 +0530

    --

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+echo "📦 Installing requirements..."
 pip install -r requirements.txt
+
+echo "🌀 Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "📦 Running migrations..."
 python manage.py migrate --noinput
+
+echo "✅ Build script completed!"
